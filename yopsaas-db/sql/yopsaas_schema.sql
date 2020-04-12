3dffc570d5889c64993dff5f8cb2117a0efe1023
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-04-12 11:43:50 +0800

    update

--- yopsaas-db/sql/yopsaas_schema.sql
+++ yopsaas-db/sql/yopsaas_schema.sql
@@ -1,8 +1,8 @@
 drop database if exists yopsaas;
-drop user if exists 'yopsaas'@'%';
+drop user 'yopsaas'@'127.0.0.1';
 -- 支持emoji：需要mysql数据库参数： character_set_server=utf8mb4
 create database yopsaas default character set utf8mb4 collate utf8mb4_unicode_ci;
 use yopsaas;
-create user 'yopsaas'@'%' identified by 'yopsaas123456';
-grant all privileges on yopsaas.* to 'yopsaas'@'%';
+create user 'yopsaas'@'127.0.0.1' identified by 'yopsaas123456';
+grant all privileges on yopsaas.* to 'yopsaas'@'127.0.0.1';
 flush privileges;

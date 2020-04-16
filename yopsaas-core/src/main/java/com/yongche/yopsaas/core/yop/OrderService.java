1ea095dfcb12f6acd93166c7e812a2869729bac9
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-04-16 14:34:24 +0800

    update

--- yopsaas-core/src/main/java/com/yongche/yopsaas/core/yop/OrderService.java
+++ yopsaas-core/src/main/java/com/yongche/yopsaas/core/yop/OrderService.java
@@ -1,15 +1,5 @@
 package com.yongche.yopsaas.core.yop;
 
-import com.yongche.yopsaas.core.config.YopProperties;
-
 public class OrderService extends BaseService {
-    private YopProperties properties;
 
-    public YopProperties getProperties() {
-        return properties;
-    }
-
-    public void setProperties(YopProperties properties) {
-        this.properties = properties;
-    }
 }

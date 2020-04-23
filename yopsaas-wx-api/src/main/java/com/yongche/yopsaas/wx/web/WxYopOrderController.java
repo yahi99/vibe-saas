5196fa040b38277cb43aa38d1524958cac14e4cf
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-04-23 15:00:41 +0800

    add order callback api

--- yopsaas-wx-api/src/main/java/com/yongche/yopsaas/wx/web/WxYopOrderController.java
+++ yopsaas-wx-api/src/main/java/com/yongche/yopsaas/wx/web/WxYopOrderController.java
@@ -1,5 +1,6 @@
 package com.yongche.yopsaas.wx.web;
 
+import com.yongche.yopsaas.core.util.ResponseUtil;
 import com.yongche.yopsaas.core.yop.OrderService;
 import com.yongche.yopsaas.wx.annotation.LoginUser;
 import com.yongche.yopsaas.wx.service.YopOrderService;
@@ -7,10 +8,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * YOP服务
@@ -34,5 +32,35 @@ public class WxYopOrderController {
     @PostMapping("create")
     public Object create(@LoginUser Integer userId, @RequestBody String body) {
         return yopOrderService.create(userId, body);
+    }
+
+    /**
+     * 回调
+     *
+     * https://github.com/yongche/developer.yongche.com/wiki/order#callback
+     *
+     * id	订单状态	加推字段	说明
+     1)	服务结束	abnormal	异常状态 详见“异常状态说明” 表
+     2)	取消	reason_id	合作方取消全部是100，易到取消为实际原因id
+     3)	调整金额	adjust_flag = 1 & abnormal
+     4)	扣款	withdraw_flag = 1
+
+     * @param yongcheOrderId 易到订单号
+     * @param yongcheOrderStatus 订单状态
+     * @param abnormal 异常状态
+     * @param reasonId 取消原因id
+     * @param adjustFlag 调整金额
+     * @param withdrawFlag 扣款
+     * @return 预估数据
+     */
+    @GetMapping("callback")
+    public Object callback(@RequestParam(defaultValue = "1", name = "yongche_order_id") String yongcheOrderId,
+                  @RequestParam(defaultValue = "4", name = "yongche_order_status") String yongcheOrderStatus,
+                  @RequestParam(defaultValue = "", name = "abnormal") String abnormal,
+                  @RequestParam(defaultValue = "", name = "reason_id") String reasonId,
+                  @RequestParam(defaultValue = "", name = "adjust_flag") String adjustFlag,
+                  @RequestParam(defaultValue = "", name = "withdraw_flag") String withdrawFlag) {
+        logger.debug(String.format("yop callback, yongche_order_id:%s, yongche_order_status:%s", yongcheOrderId, yongcheOrderStatus));
+        return ResponseUtil.ok();
     }
 }

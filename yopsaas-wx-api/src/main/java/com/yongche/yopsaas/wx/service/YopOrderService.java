921751a745d667a7b35278de6783cf1b9b4e948e
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-05-06 22:59:09 +0800

    add available service api

--- yopsaas-wx-api/src/main/java/com/yongche/yopsaas/wx/service/YopOrderService.java
+++ yopsaas-wx-api/src/main/java/com/yongche/yopsaas/wx/service/YopOrderService.java
@@ -272,6 +272,7 @@ public class YopOrderService {
         } else {
             List<String> codes = new ArrayList<String>();
             codes.add("400");
+            codes.add("499");
             codes.add("500");
             if(codes.contains(result.getCode())) {
                 status = YopsaasRideOrderService.ORDER_STATUS_CANCELLED;
